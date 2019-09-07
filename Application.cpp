06affa47d15bb23210470f0093f9d49495521199
visualstudiomachina <visualstudiomachina@outlook.com>
2019-09-07 18:34:36 +0200

    modern opengl implementantion and basic shaders

--- Application.cpp
+++ Application.cpp
@@ -1,5 +1,41 @@
+#include <Gl/glew.h>
 #include <GLFW/glfw3.h>
-#include <cmath>
+
+#include <iostream>
+
+enum ShaderType
+{
+	Vertex,
+	Fragment
+};
+
+unsigned int CompileShader(GLenum type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* cSource = source.c_str();
+	glShaderSource(id, 1, &cSource, nullptr);
+	glCompileShader(id);
+	return id;
+}
+
+
+
+unsigned int CreateShader(const std::string& vertexSource, const std::string& fragmentSource)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vertexShader = CompileShader(GL_VERTEX_SHADER, vertexSource);
+	unsigned int fragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentSource);
+
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return program;
+}
 
 int main(void)
 {
@@ -20,16 +56,64 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Error initializing glew library!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Glew initialized successfully!" << std::endl;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float positions[] =
+	{
+		-0.5f, 0.5f,
+		0.0f, -0.5f,
+		0.5f, -0.5f
+	};
+
+	unsigned int vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, positions, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	std::string vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+
+
+	// Pinky triangle!
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(0.99, 0.39, 0.6, 1.0 );\n"
+		"}\n";
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
-		glBegin(GL_POLYGON);
-		glVertex2f(-0.5f, 0.5f);
-		glVertex2f(0.0f, -0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
@@ -37,6 +121,7 @@ int main(void)
 		glfwPollEvents();
 	}
 
+	glDeleteShader(shader);
 	glfwTerminate();
 	return 0;
 }
